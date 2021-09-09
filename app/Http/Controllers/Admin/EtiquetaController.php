c9d579f4bbeda05c3fe119edcb22c0bb5bdc6378
Kyubiful <serzmr@gmail.com>
2021-09-10 01:04:06 +0200

    feat(admin): download product label

---
+++ app/Http/Controllers/Admin/EtiquetaController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Plato;
+use Barryvdh\DomPDF\Facade as PDF;
+
+class EtiquetaController extends Controller
+{
+    public function PDFEtiqueta(Request $request) 
+    {
+        $plato = Plato::where('id', $request->id)->get()[0];
+
+        $valor = $plato->plato_peso->valor;
+        $plato_info_nutricional = $plato->plato_info_nutricional;
+        
+        $alergenosPlato = [];
+
+        $alergenos = $plato->plato_alergeno;
+        foreach($alergenos as $alergeno){
+            array_push($alergenosPlato, $alergeno->nombre);
+        }
+
+        if($valor == 'M'){
+
+            $datosPlato = [
+                'nombre' => strtoupper($plato->nombre),
+                'ingredientes' => $plato->ingredientes,
+                'alergenos' => $alergenosPlato,
+                'receta' => $plato->receta,
+                'peso' => 350,
+                'energia' => $plato_info_nutricional->energia,
+                'grasas' => $plato_info_nutricional->grasas,
+                'saturadas' => $plato_info_nutricional->saturadas,
+                'carbohidratos' => $plato_info_nutricional->carbohidratos,
+                'azucares' => $plato_info_nutricional->azucar,
+                'proteinas' => $plato_info_nutricional->proteinas,
+                'sales' => $plato_info_nutricional->sodio,
+                'fibra' => $plato_info_nutricional->fibra,
+                'energia_cien' => round(($plato_info_nutricional->energia*100)/350, 2),
+                'grasas_cien' => round(($plato_info_nutricional->grasas*100)/350, 2),
+                'saturadas_cien' => round(($plato_info_nutricional->saturadas*100)/350, 2),
+                'carbohidratos_cien' => round(($plato_info_nutricional->carbohidratos*100)/350, 2),
+                'azucares_cien' => round(($plato_info_nutricional->azucar*100)/350, 2),
+                'proteinas_cien' => round(($plato_info_nutricional->proteinas*100)/350, 2),
+                'sales_cien' => round(($plato_info_nutricional->sodio*100)/350, 2),
+                'fibra_cien' => round(($plato_info_nutricional->fibra*100)/350, 2),
+
+            ];
+
+        } else if($valor == 'L'){
+
+            $datosPlato = [
+                'nombre' => strtoupper($plato->nombre),
+                'ingredientes' => $plato->ingredientes,
+                'alergenos' => $alergenosPlato,
+                'receta' => $plato->receta,
+                'peso' => 560,
+                'energia' => $plato_info_nutricional->energia,
+                'grasas' => $plato_info_nutricional->grasas,
+                'saturadas' => $plato_info_nutricional->saturadas,
+                'carbohidratos' => $plato_info_nutricional->carbohidratos,
+                'azucares' => $plato_info_nutricional->azucar,
+                'proteinas' => $plato_info_nutricional->proteinas,
+                'sales' => $plato_info_nutricional->sodio,
+                'fibra' => $plato_info_nutricional->fibra,
+                'energia_cien' => round(($plato_info_nutricional->energia*100)/560, 2),
+                'grasas_cien' => round(($plato_info_nutricional->grasas*100)/560, 2),
+                'saturadas_cien' => round(($plato_info_nutricional->saturadas*100)/560, 2),
+                'carbohidratos_cien' => round(($plato_info_nutricional->carbohidratos*100)/560, 2),
+                'azucares_cien' => round(($plato_info_nutricional->azucar*100)/560, 2),
+                'proteinas_cien' => round(($plato_info_nutricional->proteinas*100)/560, 2),
+                'sales_cien' => round(($plato_info_nutricional->sodio*100)/560, 2),
+                'fibra_cien' => round(($plato_info_nutricional->fibra*100)/560, 2),
+            ];
+
+        }
+
+        return PDF::loadView('pdf.etiquetas', $datosPlato)->setPaper('a4', 'portrait')->stream('etiqueta.pdf');
+    }
+}
---
+++ resources/views/pdf/etiquetas.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="viewport" content="width=device-width"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Mist Meals - Menú</title>
+    <link rel="icon" href="img/favicon_mistmeals.png" type="image/png">
+    <style type="text/css">
+            </style>
+</head>
+
+<body style="font-family:Verdana, Geneva, Tahoma, sans-serif;">
+    <section style="width: 80%; margin: 0 auto;">
+        <div style="border-bottom: 3px solid black;">
+            <h1 style="font-size: 30px; text-align: center;">{{ $nombre }}</h1>
+            <h3 style="text-align: center;"> PLATO UNICO </h3>
+        </div>
+        <div>
+            <p><b>INGREDIENTES </b> {{ $ingredientes }}</p>
+            <p><b>Puede contener: </b> @foreach($alergenos as $alergeno) {{ $alergeno }} @endforeach</p>
+            <p><b>Modo de preparación: </b> {{ $receta }}</p>
+            <p style="text-align: right;">Peso Neto: <b style="font-size: 24px;"> {{ $peso }}g</b></p>
+        </div>
+        <div style="margin-top: -50px;">
+            <p><b>INFORMACIÓN NUTRICIONAL</b></p>
+        </div>
+        <div>
+            <table style="width: 100%; border-collapse: collapse;">
+                <tbody>
+                    <tr>
+                        <td style="width: 40%;"></td>
+                        <td style="width: 30%; text-align: right;">(Por 100g)</td>
+                        <td style="width: 30%; text-align: right;">(Por Plato)</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">Valor Energético:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $energia_cien }} Kcal</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $energia }} Kcal</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">Grasas:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $grasas_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $grasas }} g</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">de las cuales saturadas:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $saturadas_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $saturadas }} g</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">Carbohidratos:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $carbohidratos_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $carbohidratos }} g</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">de los cuales azúcares:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $azucares_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $azucares }} g</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">Proteínas:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $proteinas_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $proteinas }} g</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">Sal:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $sales_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $sales }} g</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 2px solid black; padding: 5px;">Fibra:</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $fibra_cien }} g</td>
+                        <td style="border-bottom: 2px solid black; padding: 5px; text-align: right;">{{ $fibra }} g</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div>
+            <p style="text-align: center;"><img style="width: 400px;" src="img/otros/MistMealsLogoEtoqieta.png" alt=""></p>
+        </div>
+        <div style="border-top: 3px solid black; border-bottom: 3px solid black">
+            <p style="text-align: center; font-size: 25px;"><b>Elaborado por FUNDACIÓN JUAN XXIII</b></p>
+            <p style="text-align: center; margin-top: -20px;">Avd. Gran Vía del Este, no1 28032 Madrid</p>
+            <p style="text-align: center; margin-top: -10px; margin-bottom: 30px;">NoRGS 26.11456/M</p>
+        </div>
+        <table style="width: 100%;">
+            <tr>
+                <td style="width: 50%; text-align: center;">Lote</td>
+                <td style="width: 50%; text-align: center;">Consumir antes del</td>
+            </tr>
+        </table>
+        <div style="position: absolute; bottom: 0px;">
+            <p style="text-align: center;">Plato envasado en atmósfera protectora</p>
+        </div>
+    </section>
+</body>
+</html>
