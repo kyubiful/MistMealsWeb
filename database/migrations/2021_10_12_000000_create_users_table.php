5894eff17c46519246218db4f617ed5295c37018
Kyubiful <serzmr@gmail.com>
2021-09-15 23:55:02 +0200

    feat(discounts)

--- database/migrations/2021_10_12_000000_create_users_table.php
+++ database/migrations/2021_10_12_000000_create_users_table.php
@@ -44,7 +44,6 @@ class CreateUsersTable extends Migration
             $table->decimal('altura')->nullable();
             $table->integer('edad')->nullable();
             $table->integer('tmb')->nullable();
-            $table->string('phone')->nullable();
             $table->integer('calorias_consumidas')->nullable();
             $table->integer('calorias_propuestas')->nullable();
             $table->foreignId('sexo_id')->nullable()->constrained('sexo');
