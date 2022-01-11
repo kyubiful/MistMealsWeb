80ba08e1d6fcb529505de8deb5f3b2a565ecf582
Kyubiful <kyubiful@MacBook-Pro-M1-de-Kyubiful.local>
2022-01-11 12:41:17 +0100

    fix: cart

--- resources/views/web/carts/index.blade.php
+++ resources/views/web/carts/index.blade.php
@@ -216,9 +216,9 @@
             numberPlate.innerHTML = parseInt(numberPlate.innerHTML) + 1
             menuCartCount.innerHTML = parseInt(menuCartCount.innerHTML) + 1
             menuCartCountMobile.innerHTML = parseInt(menuCartCountMobile.innerHTML) + 1
-            productPrice.innerHTML = (parseFloat(productPrice.innerHTML.slice(0,-1).toFixed(2)) + parseFloat(data.infoPrice).toFixed(2)) + '€'
-            totalCart.innerHTML = (parseFloat(totalCart.innerHTML.slice(0,-1).toFixed(2))+parseFloat(data.infoPrice).toFixed(2))+'€'
-            subtotalCart.innerHTML = (parseFloat(subtotalCart.innerHTML.slice(0,-1).toFixed(2))+parseFloat(data.infoPrice).toFixed(2))+'€'
+            productPrice.innerHTML = (parseFloat(productPrice.innerHTML.slice(0,-1))toFixed(2) + parseFloat(data.infoPrice).toFixed(2)) + '€'
+            totalCart.innerHTML = (parseFloat(totalCart.innerHTML.slice(0,-1))toFixed(2)+parseFloat(data.infoPrice).toFixed(2))+'€'
+            subtotalCart.innerHTML = (parseFloat(subtotalCart.innerHTML.slice(0,-1))toFixed(2)+parseFloat(data.infoPrice).toFixed(2))+'€'
             totalPlates+=1
             if(totalPlates<5){
               cartMessageSection.innerHTML = '<p style="color: red; text-align: center;">*Pedido mínimo de 5 platos</p>'
@@ -278,8 +278,8 @@
               product.remove()
               menuCartCount.innerHTML = parseInt(menuCartCount.innerHTML) - 1
               menuCartCountMobile.innerHTML = parseInt(menuCartCountMobile.innerHTML) - 1
-              totalCart.innerHTML = (parseFloat(totalCart.innerHTML.slice(0,-1).toFixed(2))-parseFloat(data.infoPrice).toFixed(2))+'€'
-              subtotalCart.innerHTML = (parseFloat(subtotalCart.innerHTML.slice(0,-1).toFixed(2))-parseFloat(data.infoPrice).toFixed(2))+'€'
+              totalCart.innerHTML = (parseFloat(totalCart.innerHTML.slice(0,-1))toFixed(2)-parseFloat(data.infoPrice).toFixed(2))+'€'
+              subtotalCart.innerHTML = (parseFloat(subtotalCart.innerHTML.slice(0,-1))toFixed(2)-parseFloat(data.infoPrice).toFixed(2))+'€'
               if(totalPlates<5){
                 cartMessageSection.innerHTML = '<p style="color: red; text-align: center;">*Pedido mínimo de 5 platos</p>'
                 if(mistBtn != null){
@@ -304,12 +304,12 @@
               }
             } else {
               numberPlate.innerHTML = parseInt(numberPlate.innerHTML) - 1
-              productPrice.innerHTML = (parseFloat(productPrice.innerHTML.slice(0,-1).toFixed(2)) - parseFloat(data.infoPrice).toFixed(2)) + '€'
+              productPrice.innerHTML = (parseFloat(productPrice.innerHTML.slice(0,-1))toFixed(2) - parseFloat(data.infoPrice).toFixed(2)) + '€'
               totalPlates-=1
               menuCartCount.innerHTML = parseInt(menuCartCount.innerHTML) - 1
               menuCartCountMobile.innerHTML = parseInt(menuCartCountMobile.innerHTML) - 1
-              totalCart.innerHTML = (parseFloat(totalCart.innerHTML.slice(0,-1).toFixed(2))- parseFloat(data.infoPrice).toFixed(2))+'€'
-              subtotalCart.innerHTML = (parseFloat(subtotalCart.innerHTML.slice(0,-1).toFixed(2))- parseFloat(data.infoPrice).toFixed(2))+'€'
+              totalCart.innerHTML = (parseFloat(totalCart.innerHTML.slice(0,-1)).toFixed(2)- parseFloat(data.infoPrice).toFixed(2))+'€'
+              subtotalCart.innerHTML = (parseFloat(subtotalCart.innerHTML.slice(0,-1)).toFixed(2)- parseFloat(data.infoPrice).toFixed(2))+'€'
               if(totalPlates<5){
                 cartMessageSection.innerHTML = '<p style="color: red; text-align: center;">*Pedido mínimo de 5 platos</p>'
                 if(mistBtn != null){
